98eb707edf1d0f924c591aebda06295cc7bac770
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-27 00:02:38 +0500

    Update PlayLists.tsx

--- src/components/PlayLists.tsx
+++ src/components/PlayLists.tsx
@@ -2,24 +2,22 @@ import VideoLectureComponent from "./VideoLectureComponent";
 import type { VideoLectureType } from "../types/VideoLectureType";
 
 interface PlayListsProps {
-  data: VideoLectureType;
+  data: VideoLectureType[];
   name: string;
 }
 
 function PlayLists({ data, name }: PlayListsProps) {
   return (
-    <>
-      <div className="flex flex-col gap-2">
-        <span>{name}</span>
-        {data.map((video_lecture: VideoLectureType, i: number) => (
-          <VideoLectureComponent
-            key={video_lecture.id}
-            vidlec={video_lecture}
-            index={i + 1}
-          />
-        ))}
-      </div>
-    </>
+    <div className="flex flex-col gap-2">
+      <span className="text-2xl">{name}</span>
+      {data.map((video_lecture, i) => (
+        <VideoLectureComponent
+          key={video_lecture.id}
+          vidlec={video_lecture}
+          index={i + 1} 
+        />
+      ))}
+    </div>
   );
 }
 
